55d5b59eb07251117b9f1ba43e37e875c0e62fa8
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-06-02 15:34:47 +1200

    chnages

--- api/GetDevice/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-    context.log('Read page links from Cosmos');
-  
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: context.bindings.devices
-    };
-    
-};
